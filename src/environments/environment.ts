9513768f5e35270c55c6ec93b5d8a70679559a1c
Itta Horowitz <Ittah@noviopus.com>
2025-02-13 13:25:18 +0200

    delete openai key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   // apiKey: 'RZr95L4j0RlOIta6tbuIsAkooy1Ybrce',
   // apiKey:'4IWjH4U80bB9VwjAEgsGyzRAwjh8kkVP',
   apiKey:'HkOvd29OlZojUBhDI9hdkcuA1JbtpO2N',
-  openaiApiKey: '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'//'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'
+  openaiApiKey: ''
 };
 
 /*
